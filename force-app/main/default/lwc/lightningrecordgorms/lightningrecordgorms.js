3662dac357fc0d2169c16b94b494ad66ad3c6bb0
sagarkhamnekar <sagar@example.com>
2025-07-10 15:34:37 +0530

    lwc created

---
+++ force-app/main/default/lwc/lightningrecordgorms/lightningrecordgorms.js
@@ -0,0 +1,6 @@
+import { LightningElement, api } from 'lwc';
+
+export default class EditContactForm extends LightningElement {
+    @api recordId; // this will be passed from parent or App Builder
+
+}
