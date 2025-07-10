15e198f1ede4520d492c6964c378acbf2c6c3e3e
sagarkhamnekar <sagar@example.com>
2025-07-10 20:03:06 +0530

    lwc created

--- force-app/main/default/lwc/lightningrecordgorms/lightningrecordgorms.js
+++ force-app/main/default/lwc/lightningrecordgorms/lightningrecordgorms.js
@@ -1,6 +1,5 @@
 import { LightningElement, api } from 'lwc';
 
 export default class EditContactForm extends LightningElement {
-    @api recordId; // this will be passed from parent or App Builder
-// hello world
+    @api recordId; // This will be passed from parent or App Builder
 }
